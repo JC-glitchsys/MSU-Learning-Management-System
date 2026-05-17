ab0ecd61b023aa3db8d7857ab1173132898bb87c
Jonaidah Caris <152064487+JC-glitchsys@users.noreply.github.com>
2026-05-17 16:40:42 +0800

    Add Firestore CRUD for manage subjects (#1)

---
+++ lms-frontend/src/firebase/config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "PASTE_API_KEY",
+  authDomain: "PASTE_AUTH_DOMAIN",
+  projectId: "PASTE_PROJECT_ID",
+  storageBucket: "PASTE_STORAGE_BUCKET",
+  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
+  appId: "PASTE_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
